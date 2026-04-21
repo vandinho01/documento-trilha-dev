518ff863a46292c0ce594aeb7acdf473dda1302e
Vandinho <evandro.menshaid@gmail.com>
2026-04-20 23:19:23 -0300

    Aula 11

---
+++ semana 2/bonus Lógica/Módulo D - Dom/script.js
@@ -0,0 +1,9 @@
+console.log('O console funcionou normalmente');
+
+var vel = 60.5;
+
+if(vel > 60){
+console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+
+}
+console.log('Dirija sempre usando cinto de segurança')
