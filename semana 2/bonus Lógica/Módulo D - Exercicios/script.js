9fb70801db5ef0c6c2e7c5156fcd735cf941a89f
Vandinho <evandro.menshaid@gmail.com>
2026-04-21 21:39:59 -0300

    Aula 12 - Exercicios

---
+++ semana 2/bonus Lógica/Módulo D - Exercicios/script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas`;
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha.jpg'
+        document.body.style.background = 'blue'
+    } else if( hora >=12 && hora < 18){
+        img.src = 'tarde.jpg'
+        document.body.style.background = 'yellow'
+    } else{
+        img.src = 'noite.jpg'
+        document.body.style.background = 'red'
+    }
+  
+
+}
