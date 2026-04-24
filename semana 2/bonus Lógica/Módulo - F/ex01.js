8b41586ccd511187fea01dd03b3e264ab0249ffd
Vandinho <evandro.menshaid@gmail.com>
2026-04-24 20:56:13 -0300

    Aula 16 - exercicio 2

--- semana 2/bonus Lógica/Módulo - F/ex01.js
+++ semana 2/bonus Lógica/Módulo - F/ex01.js
@@ -1,4 +1,4 @@
-var number = document.getElementById('fnum');
+var num = document.getElementById('fnum');
 var lista = document.getElementById('flista');
 var res = document.getElementById('res')
 let valores = [];
@@ -31,7 +31,7 @@ function isNumero(n) {
     }
 }
 
-function isLista(n , l){
+function inLista(n , l){
     if(l.indexOf(Number(n)) != -1){
         return true
     } else {
@@ -41,9 +41,48 @@ function isLista(n , l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else{
-
+        alert('Valor invalido ou já encontrado na lista')
     }
+    num.value = ''
+    num.focus()
 }
 
+    function finalizar(){
+    if (valores.length == 0){
+        alert('Adicione um valor antes de finalizar.')
+    } else{
+        let tot = valores.length
+        let maior =  valores[0];
+        let menor =  valores[0];
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        } 
+
+        media = soma / tot
+
+
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}. </p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}. </p>`
+        res.innerHTML += `<p> Somando todos os valores temos ${soma}. </p>`
+        res.innerHTML += `<p> A média é ${media}. </p>`
+
+
+
+    }
+    }
